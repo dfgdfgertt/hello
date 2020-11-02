620df90790c4e69aff5918241986abe0d3b41c24
dfgdfgertt <dfgdfgertt@github.com>
2020-11-02 19:54:21 +0700

    long

---
+++ student/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    aws_table_name: 'tranngoclong',
+    aws_local_config: {
+        region: 'local',
+        endpoint: 'http://54.151.164.140:3000'
+     
+    },
+    aws_remote_config: {
+      accessKeyId: 'AKIA2MU3WQQLIUD4QNIY',
+      secretAccessKey: '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',
+      region: 'ap-southeast-1',
+    }
+}
